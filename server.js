d6e9600c063a5b98fb7b14a32c40e3b25c57d26d
Congb19 <347006087@qq.com>
2019-11-04 23:38:51 +0800

    11.4 增加视频聊天测试

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+
+
+
+
 //服务器及页面部分
 var express = require('express'),
     app = express(),
@@ -5,7 +9,13 @@ var express = require('express'),
     io = require('socket.io').listen(server),
     users=[];//保存所有在线用户的昵称
 app.use('/', express.static(__dirname));
+
+var SkyRTC = require('skyrtc').listen(server);
+var path = require("path");
+
 server.listen(8080);
+
+
 //socket部分
 io.on('connection', function(socket) {
     //昵称设置
@@ -46,6 +56,49 @@ io.on('connection', function(socket) {
         socket.broadcast.emit('newMsg', socket.nickname, msg);
     });
 });
+
+
+
+
+
+
+SkyRTC.rtc.on('new_connect', function(socket) {
+    console.log('创建新连接');
+});
+
+SkyRTC.rtc.on('remove_peer', function(socketId) {
+    console.log(socketId + "用户离开");
+});
+
+SkyRTC.rtc.on('new_peer', function(socket, room) {
+    console.log("新用户" + socket.id + "加入房间" + room);
+});
+
+SkyRTC.rtc.on('socket_message', function(socket, msg) {
+    console.log("接收到来自" + socket.id + "的新消息：" + msg);
+});
+
+SkyRTC.rtc.on('ice_candidate', function(socket, ice_candidate) {
+    console.log("接收到来自" + socket.id + "的ICE Candidate");
+});
+
+SkyRTC.rtc.on('offer', function(socket, offer) {
+    console.log("接收到来自" + socket.id + "的Offer");
+});
+
+SkyRTC.rtc.on('answer', function(socket, answer) {
+    console.log("接收到来自" + socket.id + "的Answer");
+});
+
+SkyRTC.rtc.on('error', function(error) {
+    console.log("发生错误：" + error.message);
+});
+
+
+
+
+
+
 
 
 console.log('server started');
